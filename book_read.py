10806acfdae72161d989bd3737733f9189059c1c
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2022-12-19 18:09:01 +0900

    수정

--- book_read.py
+++ book_read.py
@@ -5,12 +5,12 @@ from PIL import Image
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sb
-import altair as alt
 
 def run_book_read_app() :
     st.title('우리나라 도서관 평균 독서량 :books:')
-    st.write('2020년도 국립중앙도서관의 연간 독서량 파일을 활용하여')
+    st.write('국립중앙도서관의 연간 독서량 파일을 활용하여')
     st.write('우리나라 평균 독서량을 월별, 연령대별, 지역별로 분석했습니다')
+    st.info('2020년도의 월, 지역, 연령, 성별, 회원수, 대출건수, 독서량 컬럼을 사용했습니다')
     image_url = 'https://cdn.pixabay.com/photo/2017/07/02/09/03/books-2463779_960_720.jpg'
     st.image(image_url, use_column_width=True)
 
@@ -21,15 +21,14 @@ def run_book_read_app() :
 
     if status == '월별 평균' :
         st.subheader('월별 독서량 평균')
-        MONTH = df.groupby('월')['독서량'].mean()
-        MONTH = MONTH.to_frame()
+        MONTH = df.groupby('월')['독서량'].mean().to_frame()
         max_MONTH = MONTH.loc[ MONTH['독서량'] == MONTH['독서량'].max() ].index.tolist()[0]
         st.info('평균 독서율이 제일 높은 월은 ' + str(max_MONTH) + '월 입니다')
         st.bar_chart(MONTH, height=600)
+
     elif status == '연령대별 평균' :
         st.subheader('연령대별 독서량 평균')
-        AGE = df.groupby('연령')['독서량'].mean()
-        AGE = AGE.to_frame()
+        AGE = df.groupby('연령')['독서량'].mean().to_frame()
         max_AGE = AGE.loc[ AGE['독서량'] == AGE['독서량'].max() ].index.tolist()[0]
         st.info('평균 독서율이 제일 높은 연령대는 ' + max_AGE + '입니다')
         st.bar_chart(AGE, height=600)
@@ -41,7 +40,7 @@ def run_book_read_app() :
             choice_Month = MONTH_AGE.loc[ MONTH_AGE['월'] == my_choice ]
             choice_Month = choice_Month.groupby('연령')['독서량'].mean().to_frame()
         
-            fig1 = px.bar(choice_Month, y='독서량', height=600, title= str(my_choice)+ '월 연령대별 평균 독서량')
+            fig1 = px.bar(choice_Month, y='독서량', height=600, title= str(my_choice)+ '월 연령대별 평균 독서량', color='독서량', color_continuous_scale='plotly3')
             st.plotly_chart(fig1)
     st.subheader('')
 
@@ -49,7 +48,7 @@ def run_book_read_app() :
     
     AREA_AGEdf = df.groupby('연령')[['회원수','대출건수']].sum()
     AREA_AGEdf = AREA_AGEdf.reset_index()
-    fig2 = px.bar(AREA_AGEdf, x= '연령', y=['회원수','대출건수'], barmode='group', height=600)
+    fig2 = px.bar(AREA_AGEdf, x= '연령', y=['회원수','대출건수'], barmode='group', height=600, color_continuous_scale='plotly3')
     st.plotly_chart(fig2)
     st.dataframe(AREA_AGEdf)
     st.subheader('')
@@ -60,35 +59,35 @@ def run_book_read_app() :
     if status == '지역별 평균 독서량' :
         AREA_READQY = df.groupby('지역')['독서량'].mean().to_frame()
 
-        fig3 = px.bar(AREA_READQY, y='독서량', height=600, title= '지역별 평균 독서량')
-        st.plotly_chart(fig3)
         AREA_READQY = AREA_READQY.reset_index()
         max_AREA_READQY = AREA_READQY.loc[ AREA_READQY['독서량'] == AREA_READQY['독서량'].max() ]
         st.info('평균 독서량이 가장 높은 지역은 ' + max_AREA_READQY['지역'].tolist()[0] + '입니다')
+        fig3 = px.bar(AREA_READQY, y='독서량', height=600, title= '지역별 평균 독서량', color='독서량', color_continuous_scale='plotly3')
+        st.plotly_chart(fig3)
 
     elif status == '지역별 회원수' :
         AREA_LON = df.groupby('지역')['회원수'].sum().to_frame().reset_index()
 
-        fig4 = px.pie(AREA_LON, '지역', '회원수', title='지역별 회원수', hole=.3, )
-        st.plotly_chart(fig4)
         max_AREA_LON = AREA_LON.loc[ AREA_LON['회원수'] == AREA_LON['회원수'].max() ]
         st.info('회원수가 가장 많은 지역은 ' + max_AREA_LON['지역'].tolist()[0] + '입니다')
+        fig4 = px.pie(AREA_LON, '지역', '회원수', title='지역별 회원수', hole=.3)
+        st.plotly_chart(fig4)
 
     elif status == '지역별 대출건수' :
         AREA_LONMBER = df.groupby('지역')['대출건수'].sum().to_frame().reset_index()
 
-        fig5 = px.pie(AREA_LONMBER, '지역', '대출건수', title='지역별 대출건수', hole=.3)
-        st.plotly_chart(fig5)
         max_AREA_LONMBER = AREA_LONMBER.loc[ AREA_LONMBER['대출건수'] == AREA_LONMBER['대출건수'].max() ]
         st.info('대출건수가 가장 많은 지역은 ' + max_AREA_LONMBER['지역'].tolist()[0] + '입니다')
+        fig5 = px.pie(AREA_LONMBER, '지역', '대출건수', title='지역별 대출건수', hole=.3)
+        st.plotly_chart(fig5)
 
     if st.checkbox('각 지역별 세부 데이터 보기') :
         my_choice = st.selectbox('지역을 선택하세요', df['지역'].unique())
         if my_choice != 0 :
             df2 = df[['지역','연령','회원수','대출건수']]
             df2 = df2.loc[ df2['지역'] == my_choice ]
+            st.info(my_choice + '의 총 회원 수는' + str(df2['회원수'].sum()) + '명 입니다')
             st.dataframe(df2.groupby('연령')[['회원수','대출건수']].sum())
-            st.write(my_choice + '의 총 회원 수는' + str(df2['회원수'].sum()) + '명 입니다')
 
 
 
--- library_location.py
+++ library_location.py
@@ -13,31 +13,30 @@ def run_library_location_app() :
     st.image(image_url, use_column_width=True)
 
     df = pd.read_csv('LIBRARY_202211.csv',index_col=0)
-    df = df.dropna()
     st.subheader('')
 
     st.subheader('전국 도서관 정보')
-    df_count = df['ONE_AREA_NM'].count()
-    fig1 = px.bar(y= df['ONE_AREA_NM'], height=600, title= '전국 공공 도서관 갯수 (총 ' + str(df_count) + '개)')
+    df_count = df['지역'].count()
+    fig1 = px.bar(y= df['지역'], height=600, title= '전국 공공 도서관 갯수 (총 ' + str(df_count) + '개)')
     st.plotly_chart(fig1)
-    st.dataframe(df[['ONE_AREA_NM', 'LBRRY_NM', 'LBRRY_ADDR']])
+    st.dataframe(df[['지역', '도서관명', '주소']])
     
     if st.checkbox('도서관명으로 검색하기') :
         library_name = st.text_input('검색할 도서관명을 입력해주세요')
-        library_name = df.loc[ df['LBRRY_NM'].str.contains(library_name) ]
-        st.dataframe(library_name[['ONE_AREA_NM', 'LBRRY_NM', 'LBRRY_ADDR']])
+        library_name = df.loc[ df['도서관명'].str.contains(library_name) ]
+        st.dataframe(library_name[['지역', '도서관명', '주소']])
     st.subheader('')
 
     st.subheader('각 지역별 도서관 정보')
-    AREA_choice1 = st.selectbox('지역을 선택하세요', df['ONE_AREA_NM'].unique())
-    df2 = df.loc[ df['ONE_AREA_NM'] == AREA_choice1 ]
-    AREA_choice2 = st.selectbox('세부 지역을 선택하세요', df2['TWO_AREA_NM'].unique())
+    AREA_choice1 = st.selectbox('지역을 선택하세요', df['지역'].unique())
+    df2 = df.loc[ df['지역'] == AREA_choice1 ]
+    AREA_choice2 = st.selectbox('세부 지역을 선택하세요', df2['세부지역'].unique())
 
-    AREA_choicedf = df.loc[ (df['ONE_AREA_NM'] == AREA_choice1) & (df['TWO_AREA_NM'] == AREA_choice2) ]
-    st.dataframe(AREA_choicedf[['LBRRY_TY_NM', 'ONE_AREA_NM', 'LBRRY_NM', 'LBRRY_ADDR']])
+    AREA_choicedf = df.loc[ (df['지역'] == AREA_choice1) & (df['세부지역'] == AREA_choice2) ]
+    st.dataframe(AREA_choicedf[['분류', '지역', '도서관명', '주소']])
 
     st.map(AREA_choicedf)
-    AREAdf = df.loc[ df['ONE_AREA_NM'] == AREA_choice1 ]
-    AREAdf_count = AREAdf['ONE_AREA_NM'].count()
-    fig2 = px.bar(AREAdf, y= 'TWO_AREA_NM', height=600, title= AREA_choice1 + '의 공공 도서관 갯수(총 ' + str(AREAdf_count) + '개)')
+    AREAdf = df.loc[ df['지역'] == AREA_choice1 ]
+    AREAdf_count = AREAdf['지역'].count()
+    fig2 = px.bar(AREAdf, y= '세부지역', height=600, title= AREA_choice1 + '의 공공 도서관 갯수(총 ' + str(AREAdf_count) + '개)')
     st.plotly_chart(fig2)
